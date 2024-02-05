fcd9e25a718a7a033355bafa109dd2073a98c636
Henoc Soares Freire <henocsoares@hotmail.com.br>
2024-02-05 20:55:12 -0300

    Task!

--- src/index.js
+++ src/index.js
@@ -1,37 +1,200 @@
-// Import required modules
-const fs = require('fs');
-const os = require('os');
-const crypto = require('crypto');
-const zlib = require('zlib');
+import fs from 'fs';
+import os from 'os';
+import crypto from 'crypto';
+import zlib from 'zlib';
 
-// Parse command-line arguments
 const args = process.argv.slice(2);
 const usernameIndex = args.indexOf('--username');
 const username = args[usernameIndex + 1];
 console.log(`Welcome to the File Manager, ${username}!`);
 
-// Define the main function to handle user commands
-function main() {
-    // Implement the logic to handle user commands
-    // Use a switch statement or if-else statements to execute the corresponding operation
-    // Based on the user input, call the appropriate functions to perform file operations, OS info, hash calculation, etc.
-    // Ensure proper error handling and provide feedback to the user
-}
+const main = () => {
+    process.stdin.setEncoding('utf-8');
+    process.stdin.on('data', (input) => {
+        const [command, ...params] = input.trim().split(' ');
+        executeCommand(command, params);
+    });
+    console.log(`You are currently in ${process.cwd()}`);
+};
 
-// Execute the main function
+const executeCommand = (command, params) => {
+    switch (command) {
+        case 'up':
+            navigateUp();
+            break;
+        case 'cd':
+            navigateToDirectory(params[0]);
+            break;
+        case 'ls':
+            listDirectory();
+            break;
+        case 'cat':
+            readFile(params[0]);
+            break;
+        case 'add':
+            createFile(params[0]);
+            break;
+        case 'rn':
+            renameFile(params[0], params[1]);
+            break;
+        case 'cp':
+            copyFile(params[0], params[1]);
+            break;
+        case 'mv':
+            moveFile(params[0], params[1]);
+            break;
+        case 'rm':
+            deleteFile(params[0]);
+            break;
+        case 'os':
+            getOSInfo(params[0]);
+            break;
+        case 'hash':
+            calculateHash(params[0]);
+            break;
+        case 'compress':
+            compressFile(params[0], params[1]);
+            break;
+        case 'decompress':
+            decompressFile(params[0], params[1]);
+            break;
+        default:
+            console.log('Invalid input');
+    }
+};
+
+const navigateUp = () => {
+    const currentPath = process.cwd();
+    const parentPath = path.resolve(currentPath, '..');
+    if (parentPath !== currentPath) {
+        process.chdir(parentPath);
+        console.log(`You are currently in ${parentPath}`);
+    }
+};
+
+const navigateToDirectory = (directory) => {
+    fs.readdir(directory, (err, files) => {
+        if (err) {
+            console.log('Operation failed');
+        } else {
+            process.chdir(directory);
+            console.log(`You are currently in ${directory}`);
+        }
+    });
+};
+
+const listDirectory = () => {
+    fs.readdir(process.cwd(), (err, files) => {
+        if (err) {
+            console.log('Operation failed');
+        } else {
+            const sortedFiles = files.sort();
+            console.log('| **Index** | **Name**            | **Type**     |');
+            console.log('|-----------|---------------------|--------------|');
+            sortedFiles.forEach((file, index) => {
+                const stats = fs.statSync(file);
+                const type = stats.isDirectory() ? 'directory' : 'file';
+                console.log(`| ${index}         | '${file}'              | '${type}'  |`);
+            });
+        }
+    });
+};
+
+const readFile = (filePath) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.log('Operation failed');
+        } else {
+            console.log(data);
+        }
+    });
+};
+
+const createFile = (fileName) => {
+    fs.writeFile(fileName, '', (err) => {
+        if (err) {
+            console.log('Operation failed');
+        }
+    });
+};
+
+const renameFile = (oldName, newName) => {
+    fs.rename(oldName, newName, (err) => {
+        if (err) {
+            console.log('Operation failed');
+        }
+    });
+};
+
+const copyFile = (source, destination) => {
+    fs.copyFile(source, destination, (err) => {
+        if (err) {
+            console.log('Operation failed');
+        }
+    });
+};
+
+const moveFile = (source, destination) => {
+    fs.rename(source, destination, (err) => {
+        if (err) {
+            console.log('Operation failed');
+        }
+    });
+};
+
+const deleteFile = (fileName) => {
+    fs.unlink(fileName, (err) => {
+        if (err) {
+            console.log('Operation failed');
+        }
+    });
+};
+
+const getOSInfo = (param) => {
+    switch (param) {
+        case '--EOL':
+            console.log(os.EOL);
+            break;
+        case '--cpus':
+            console.log(os.cpus());
+            break;
+        case '--homedir':
+            console.log(os.homedir());
+            break;
+        case '--username':
+            console.log(os.userInfo().username);
+            break;
+        case '--architecture':
+            console.log(process.arch);
+            break;
+        default:
+            console.log('Invalid input');
+    }
+};
+
+const calculateHash = (filePath) => {
+    const hash = crypto.createHash('sha256');
+    const stream = fs.createReadStream(filePath);
+    stream.on('data', (data) => {
+        hash.update(data);
+    });
+    stream.on('end', () => {
+        console.log(hash.digest('hex'));
+    });
+};
+
+const compressFile = (source, destination) => {
+    const gzip = zlib.createGzip();
+    const readStream = fs.createReadStream(source);
+    const writeStream = fs.createWriteStream(destination);
+    readStream.pipe(gzip).pipe(writeStream);
+};
+
+const decompressFile = (source, destination) => {
+    const gunzip = zlib.createGunzip();
+    const readStream = fs.createReadStream(source);
+    const writeStream = fs.createWriteStream(destination);
+    readStream.pipe(gunzip).pipe(writeStream);
+};
+
 main();
-
-// Function to print the current working directory
-function printCurrentDirectory() {
-    const cwd = process.cwd();
-    console.log(`You are currently in ${cwd}`);
-}
-
-// Function to handle the .exit command and exit gracefully
-process.on('SIGINT', () => {
-    console.log(`Thank you for using File Manager, ${username}, goodbye!`);
-    process.exit();
-});
-
-// Implement the rest of the functionalities based on the assignment requirements
-// Implement functions for file operations, OS info, hash calculation, compression, etc.
